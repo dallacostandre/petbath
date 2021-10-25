bcf28d36866624896234b8b112eb382d4f114eeb
Andre <dallacosta.andre@gmail.com>
2021-10-25 17:30:43 -0300

    acessar e login

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,18 @@ use App\Models\Cliente;
 use Illuminate\Support\Facades\Route;
 
 
-Route::GET('/', function () {
+Route::GET('/acessar', function () {
     return view('auth.login');
 }); 
 
-/* Route::GET('/', function () {
+Route::GET('/cadastro', function () {
+    return view('auth.register');
+}); 
+
+
+ Route::GET('/', function () {
     return view('landing_page');
-}); */
+});
 
 
 Route::group(['middleware' => 'auth'], function () {
