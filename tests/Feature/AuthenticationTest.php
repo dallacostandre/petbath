4aafe30c94a2a92c8f53f96f7475d57017137cf2
dallacostandre <dallacosta.andre@gmail.com>
2021-11-22 18:41:13 -0300

    ok

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -13,7 +13,7 @@ class AuthenticationTest extends TestCase
 
     public function test_login_screen_can_be_rendered()
     {
-        $response = $this->get('/login');
+        $response = $this->get('/acessar');
 
         $response->assertStatus(200);
     }
@@ -22,7 +22,7 @@ class AuthenticationTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $response = $this->post('/login', [
+        $response = $this->post('/acessar', [
             'email' => $user->email,
             'password' => 'password',
         ]);
