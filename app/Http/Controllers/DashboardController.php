aaefd2435555ad09e484bab4d694dd2135cbdcf7
André <andre@creatifdigital.com.br>
2022-05-17 09:42:49 -0300

    alteracao nome das telas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.Main.index');
+        return view('dashboard.main.index');
     }
 }
